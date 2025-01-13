37bf1e575a1e6ccdaa42054119f7ac36c1bf57ec
Swati <swatimishra0209@gmail.com>
2025-01-13 18:19:24 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-# from networksecurity.components.data_ingestion import DataIngestion
-# from networksecurity.exception.exception import NetworkSecurityException
-# from networksecurity.logging.logger import logging
-# import sys
-# from networksecurity.entity.config_entity import DataIngestionConfig
-# from networksecurity.entity.config_entity import TrainingPipelineConfig
-
-
-# if __name__ == "__main__":
-#     try:
-#         trainingpipelineconfig = TrainingPipelineConfig()
-#         dataingestionconfig = DataIngestionConfig(TrainingPipelineConfig)
-#         dataingestion = DataIngestion(dataingestionconfig)
-#         logging.info("Initiated the data ingestion")
-#         dataingestionartifact = dataingestion.initiate_data_ingestion()
-#         print(dataingestionartifact)
-
-#     except Exception as e:
-#         raise  NetworkSecurityException(e, sys)
-
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
@@ -38,6 +19,12 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed!")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise  NetworkSecurityException(e, sys)
-        
--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -27,23 +27,23 @@ class DataIngestion:
             raise NetworkSecurityException(e,sys)
         
     def export_collection_as_dataframe(self):
-        
-        #Read data from mongodb
-         
         try:
-            database_name=self.data_ingestion_config.database_name
-            collection_name=self.data_ingestion_config.collection_name
-            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
-            collection=self.mongo_client[database_name][collection_name]
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingestion_config.collection_name
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            collection = self.mongo_client[database_name][collection_name]
 
-            df=pd.DataFrame(list(collection.find()))
-            if "_id" in df.columns.to_list():
-                df=df.drop(columns=["_id"],axis=1)
-            
-            df.replace({"na":np.nan},inplace=True)
+            df = pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns:
+                df = df.drop(columns=["_id"], errors="ignore")
+        
+            if df.empty:
+                raise NetworkSecurityException("The MongoDB collection is empty.", sys)
+        
+            df.replace({"na": np.nan}, inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e, sys)
         
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
@@ -98,4 +98,4 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException (e,sys)
